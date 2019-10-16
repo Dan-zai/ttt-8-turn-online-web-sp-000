5367107bb3235fa41b7be6f83c1a4553d24da812
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2019-10-16 04:57:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,18 @@
+
 def display_board(board)
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+  
+
+def input_to_index(index)
+  index.to_i - 1 
 end
 
-
 def valid_move?(board, index)
-  def valid_move?(board, index)
   move = index.to_i - 1
   if !position_taken?(board, index) && index.between?(0,8)
     true
@@ -21,7 +30,14 @@ end
 end
 
 
-def move(board, index, token)
+def move(board, input_to_index, token = "X")
+  board[input_to_index] = token
 end
 
-  
+def turn(board)
+  puts "Please enter 1-9:"
+end
+
+
+
+
